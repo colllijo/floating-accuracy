159d54499e088f0291adda834b8cd3e47dc75082
Liam Metzger <liam.metzger@bluewin.ch>
2024-08-20 10:57:51 +0200

    Add Apache Commons Math3 for BigReal

--- src/main/java/dev/coll/App.java
+++ src/main/java/dev/coll/App.java
@@ -2,6 +2,8 @@ package dev.coll;
 
 import java.util.LinkedList;
 
+import org.apache.commons.math3.util.BigReal;
+
 import dev.coll.math.Calculation;
 import dev.coll.math.CalculationResult;
 import dev.coll.math.CalculationStep;
@@ -9,6 +11,12 @@ import dev.coll.math.CalculationStep;
 public class App {
   public static void main(String[] args) {
     String expression = "({0} + {1}) * {2}";
+
+    BigReal real1 = new BigReal("1");
+    BigReal real3 = new BigReal("3");
+    BigReal res = real1.divide(real3);
+
+    System.out.println(res.bigDecimalValue().toString());
 
     Calculation calc = new Calculation(expression);
     CalculationResult result = calc.evaluate(new String[] { "0.1", "0.2", "2" });
