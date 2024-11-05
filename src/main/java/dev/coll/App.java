eb58204220af6ef3f41ae0491b999aab06849aa7
LinuxKaliUser <arunan.nagarajah@bbzbl-it.ch>
2024-11-05 12:05:12 +0100

    Added Expression TODO: Add the Expression parameters later

--- src/main/java/dev/coll/App.java
+++ src/main/java/dev/coll/App.java
@@ -16,13 +16,25 @@ public class App {
   public static void main(String[] args) throws IOException {
     final int SAMPLE_SIZE = 1000000;
     final String RESULT_DIRECTORY = "analytics";
+    final double GRAVITATIONAL_CONSTANT = 6.67430e-11; // m³/kg·s²
+    final double COULOMB_CONSTANT = 8.9875517923e9; // N·m²/C²
+    // TODO: Add the Expression parameters
 
     Map<String, String> expressions = Map.of(
-            "default", "({0} + {1}) * {2} / {3}"
+            "Kinematik", "({0} + {1}) * {2} / {3}",
+            "Gravitation", "(6.67430e-11 *({0} * {1}))/ (({2} +{3})*({2} +{3}))",
+            "Colombo", "(8.9875517923e9 *({0} * {1}))/ ({2} *{2})"
     );
     Map<String, CalculationParameter[]> params = Map.of(
-            "default", new CalculationParameter[] {
+            "Kinematik", new CalculationParameter[] {
                     new CalculationParameter(-100, 100, false),
+                    new CalculationParameter(20.02),
+                    new CalculationParameter(0, 200, false),
+                    new CalculationParameter(-100, 100, true)
+            },
+            "Gravitation",new CalculationParameter[] {
+                    new CalculationParameter(1e23, 1e28, false) //Min: Merkur masse /3 Max: Unter Braune Zwerg Masse 13 Jupitermasse
+                    ,
                     new CalculationParameter(20.02),
                     new CalculationParameter(0, 200, false),
                     new CalculationParameter(-100, 100, true)
